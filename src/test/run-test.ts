7152d994205084a957344ec6c26847587e372f4c
svipas <benas.svipas@gmail.com>
2019-07-11 00:07:57 +0300

    Update all dependencies

--- src/test/run-test.ts
+++ src/test/run-test.ts
@@ -4,12 +4,12 @@ import { runTests } from 'vscode-test';
 (async function main() {
   // The folder containing the Extension Manifest package.json
   // Passed to `--extensionDevelopmentPath`
-  const extensionPath = process.cwd();
+  const extensionDevelopmentPath = process.cwd();
 
   // The path to test runner
   // Passed to --extensionTestsPath
-  const testRunnerPath = path.join(__dirname, './suite');
+  const extensionTestsPath = path.join(__dirname, './suite');
 
   // Download VS Code, unzip it and run the integration test
-  await runTests({ extensionPath, testRunnerPath });
+  await runTests({ extensionDevelopmentPath, extensionTestsPath });
 })();
